e5ad3373acca524b1399f15b72f4ed8e752cf0af
gejiang <632651021@qq.com>
2023-06-28 16:43:26 +0800

    first commit

---
+++ cmake-build-debug-/CMakeFiles/webserver.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/data/gj/webserver/test_presure/webbench-1.5/socket.c" "/home/data/gj/webserver/cmake-build-debug-/CMakeFiles/webserver.dir/test_presure/webbench-1.5/socket.c.o"
+  "/home/data/gj/webserver/test_presure/webbench-1.5/webbench.c" "/home/data/gj/webserver/cmake-build-debug-/CMakeFiles/webserver.dir/test_presure/webbench-1.5/webbench.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../CGImysql"
+  "../http"
+  "../lock"
+  "../log"
+  "../threadpool"
+  "../timer"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/data/gj/webserver/CGImysql/sign.cpp" "/home/data/gj/webserver/cmake-build-debug-/CMakeFiles/webserver.dir/CGImysql/sign.cpp.o"
+  "/home/data/gj/webserver/CGImysql/sql_connection_pool.cpp" "/home/data/gj/webserver/cmake-build-debug-/CMakeFiles/webserver.dir/CGImysql/sql_connection_pool.cpp.o"
+  "/home/data/gj/webserver/config.cpp" "/home/data/gj/webserver/cmake-build-debug-/CMakeFiles/webserver.dir/config.cpp.o"
+  "/home/data/gj/webserver/http/http_conn.cpp" "/home/data/gj/webserver/cmake-build-debug-/CMakeFiles/webserver.dir/http/http_conn.cpp.o"
+  "/home/data/gj/webserver/log/log.cpp" "/home/data/gj/webserver/cmake-build-debug-/CMakeFiles/webserver.dir/log/log.cpp.o"
+  "/home/data/gj/webserver/main.cpp" "/home/data/gj/webserver/cmake-build-debug-/CMakeFiles/webserver.dir/main.cpp.o"
+  "/home/data/gj/webserver/timer/lst_timer.cpp" "/home/data/gj/webserver/cmake-build-debug-/CMakeFiles/webserver.dir/timer/lst_timer.cpp.o"
+  "/home/data/gj/webserver/webserver.cpp" "/home/data/gj/webserver/cmake-build-debug-/CMakeFiles/webserver.dir/webserver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../CGImysql"
+  "../http"
+  "../lock"
+  "../log"
+  "../threadpool"
+  "../timer"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/webserver.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webserver.dir/CGImysql/sign.cpp.o"
+  "CMakeFiles/webserver.dir/CGImysql/sql_connection_pool.cpp.o"
+  "CMakeFiles/webserver.dir/config.cpp.o"
+  "CMakeFiles/webserver.dir/http/http_conn.cpp.o"
+  "CMakeFiles/webserver.dir/log/log.cpp.o"
+  "CMakeFiles/webserver.dir/main.cpp.o"
+  "CMakeFiles/webserver.dir/test_presure/webbench-1.5/socket.c.o"
+  "CMakeFiles/webserver.dir/test_presure/webbench-1.5/webbench.c.o"
+  "CMakeFiles/webserver.dir/timer/lst_timer.cpp.o"
+  "CMakeFiles/webserver.dir/webserver.cpp.o"
+  "webserver"
+  "webserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/webserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
